ce1a579c71dc59cdb8ae1eff5dc7016bf9de696b
Nabeel Hassan <nabeelhassan1998@gmail.com>
2020-08-12 00:13:53 +0500

    Scroll View Test

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import React from "react";
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  Button,
+  Image,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+
+import Test from "./assets/Test";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello World !!!</Text>
+      <Button
+        color="white"
+        title="Check Request(s)"
+        onPress={() => {
+          Alert.alert("Friend Request", "You have a new friend request", [
+            { text: "Accept", onPress: () => Alert.alert("Request Accepted") },
+            { text: "Reject", onPress: () => Alert.alert("Request Rejected") },
+            { text: "Cancel" },
+          ]);
+        }}
+      />
+      <Button
+        color="white"
+        title="Enter Your Age"
+        onPress={() => {
+          Alert.prompt("Your Age", "Please Enter your Age here", (text) =>
+            alert("Your Age is " + text)
+          );
+        }}
+      />
+      <ScrollView>
+        <Test />
+      </ScrollView>
+      <Image source={require("./assets/favicon.png")} />
     </SafeAreaView>
   );
 }
--- assets/Test.js
+++ assets/Test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {
   StyleSheet,
-  Text,
   SafeAreaView,
   View,
   Button,
@@ -17,30 +16,47 @@ import {
 export default function Test() {
   return (
     <SafeAreaView style={styles.container}>
-      <Button
-        color="white"
-        title="Check Request(s)"
-        onPress={() => {
-          Alert.alert("Friend Request", "You have a new friend request", [
-            { text: "Accept", onPress: () => Alert.alert("Request Accepted") },
-            { text: "Reject", onPress: () => Alert.alert("Request Rejected") },
-            { text: "Cancel" },
-          ]);
-        }}
-      />
-      <Button
-        color="white"
-        title="Enter Your Age"
-        onPress={() => {
-          Alert.prompt("Your Age", "Please Enter your Age here", (text) =>
-            alert("Your Age is " + text)
-          );
-        }}
-      />
-      <Image source={require("./assets/favicon.png")} />
       <View
         style={{
           flexDirection: "row",
+          justifyContent: "space-around",
+        }}
+      >
+        <TouchableWithoutFeedback
+          onPress={() => {
+            alert("Image Clicked");
+          }}
+        >
+          <Image
+            source={{
+              width: 100,
+              height: 150,
+              uri: "https://picsum.photos/200/300?random=1",
+            }}
+          />
+        </TouchableWithoutFeedback>
+        <TouchableOpacity>
+          <Image
+            source={{
+              width: 100,
+              height: 150,
+              uri: "https://picsum.photos/200/300?random=2",
+            }}
+          />
+        </TouchableOpacity>
+        <TouchableHighlight onPress={() => console.log("Tapped")}>
+          <Image
+            source={{
+              width: 100,
+              height: 150,
+              uri: "https://picsum.photos/200/300?random=3",
+            }}
+          />
+        </TouchableHighlight>
+      </View>
+      <View
+        style={{
+          paddingTop: 20,
           justifyContent: "space-around",
         }}
       >
